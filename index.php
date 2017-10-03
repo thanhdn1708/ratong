ff2eceea1af035323997a502b23d2bb80a278f42
thanhdn1708 <thanhdn1708@gmail.com>
2017-10-03 22:22:15 +0700

    myssql
    213

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 10/3/2017
- * Time: 5:17 PM
- */
+$servername = "172.31.58.54";
+$username = "admin";
+$password = "123456";
 
-echo 'thanhd';
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
